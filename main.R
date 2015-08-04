06e6c081b82c7fa18e898f2a6cd65eca17ea1f36
markhagemann <mark.hagemann@gmail.com>
2015-08-04 16:51:38 -0400

    Improvements to main file

--- main.R
+++ main.R
@@ -3,35 +3,40 @@
 # package deals
 
 library(devtools)
-
 library(assertthat)
 library(testthat)
 library(dplyr)
 library(mgcv)
 library(conditionalSample)
 
+devtools::load_all()
+
+
 # package documentation
 
+# setup package utilities
 use_package("assertthat")
-# use_package("testthat")
+use_package("testthat")
 use_package("dplyr")
 use_package("mgcv")
 use_package("conditionalSample")
+use_package("nlme")
+use_testthat()
+
+# document
 devtools::document()
 
 
-# set up to use testthat for tests:
+# test
+devtools::test()
+
 
 # set up to use data
-
-dir.create("data")
-?devtools::use_data
-
-load("../../Research/ExtremeEvents/cache/samplePData.Rdata")
+# dir.create("data")
 use_data(sampleData, overwrite = TRUE)
-
 
 # load and install package
 
-devtools::load_all()
+
+
 devtools::install()
