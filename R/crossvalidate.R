797b07d8aec38d62421d5e1bafb2f92d5cb12015
markwh <mark.hagemann@gmail.com>
2015-08-10 13:25:59 -0400

    Implement crossvalidation as its own method for rcgams

--- R/crossvalidate.R
+++ R/crossvalidate.R
@@ -1,3 +1,58 @@
+#' @export
+
+crossvalidate <- function(object, ...) {
+  UseMethod("crossvalidate")
+}
+
 
 #' @export
-# crossvalidate.rcgam <- function(object, kfolds = 0, s)
+crossvalidate.rcgam  <- function(object, kfolds = 0,
+                                 statistic = c("R2", "mse", "mae", "rmse")) {
+
+  data <- getData(object)
+  fmla <- object$formula
+
+  if(kfolds == 0)
+    kfolds <- nrow(data)
+
+  yname <- object$yname # sneaky back door for objects of my own design
+
+  yvar <- var(data[[yname]])
+
+  statistic = match.arg(statistic)
+  sfun = ifelse(statistic == "mae", "mae", "sse")
+
+  # split data into folds
+  case.folds = sample(rep(1:kfolds, length.out = nrow(data)))
+  # fold.ss = rep(NA_real_, kfolds) # sum of squared residuals for each fold
+  # fold.mae = rep(NA_real_, kfolds) # mean absolute error for each fold
+  fold.stat = rep(NA_real_, kfolds) # mean absolute error for each fold
+
+  for (fold in 1:kfolds) {
+    train <- data[case.folds != fold, ]
+    test <- data[case.folds == fold, ]
+    curobj <- do.call("rcgam", list(formula = fmla, data = train))
+
+    ypred <- as.numeric(predict(curobj, newdata = test))
+    ymeas <- test[[yname]]
+
+    # fold.ss[fold] <- sum((ymeas - ypred)^2)
+    fold.stat[fold] <- do.call(sfun, list(ymeas, ypred))
+  }
+
+  # assemble folds
+  # r2 <- 1 - sum(fold.ss) / sum((ymeas - mean(ymeas, na.rm = TRUE))^2)
+  ymeas <- data[[yname]]
+  ssum <- sum(fold.stat)
+
+  if(statistic == "mae")
+    sagg = ssum / kfolds
+  else if (statistic == "mse")
+    sagg = ssum / nrow(data)
+  else if (statistic == "rmse")
+    sagg = sqrt(ssum / nrow(data))
+  else
+    sagg = 1 - ssum / sum((ymeas - mean(ymeas, na.rm = TRUE))^2)
+
+  sagg
+}
--- tests/testthat/test-crossvalidation.R
+++ tests/testthat/test-crossvalidation.R
@@ -6,6 +6,9 @@ test_that("crossvalidation works for rcgams", {
   foo = makeModelData(Phosphorus)
   mod2 = rcgam(c ~ s(q) + s(doy, bs = "cc", k = 4) + s(time), foo)
 
+  expect_is(crossvalidate(mod2), "numeric")
+  expect_is(crossvalidate(mod2, kfolds = 5), "numeric")
+
   expect_less_than(crossvalidate(mod2), 1)
   expect_less_than(crossvalidate(mod2, kfolds = 5), 1)
 })
