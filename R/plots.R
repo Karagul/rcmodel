30f2b50a20762cfeed48bfccd89a1a584a1cf8f7
markwh <mark.hagemann@gmail.com>
2016-11-25 18:07:55 -0500

    exported ggtermplot method

--- R/plots.R
+++ R/plots.R
@@ -2,7 +2,7 @@
 # Plotting functions for rcmodel objects
 
 #' @importFrom markstats ggTermPlot
-#'
+#' @export
 ggTermPlot.rcgam <- function(object, xvar, data = NULL, ...) {
   obj <- as.gam(object)
   out <- ggTermPlot(obj, xvar, data, ...)
